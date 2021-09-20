3e79c46d6c331800034d9f10e29529376a8d4210
Aditya Thakur <aditya.thakur.261099@gmail.com>
2021-09-20 23:29:43 +0530

    feat (proto) : Add new task functionality complete

--- src/app/shared/form-drawer/form-drawer.component.ts
+++ src/app/shared/form-drawer/form-drawer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { StoreService } from '../store/store.service';
 import { StreamEvent, StreamServiceService } from '../stream-service/stream-service.service';
 
 @Component({
@@ -10,7 +12,10 @@ import { StreamEvent, StreamServiceService } from '../stream-service/stream-serv
 })
 export class FormDrawerComponent implements OnInit, OnDestroy {
 
-  constructor(private streamService: StreamServiceService) { }
+  constructor (
+    private streamService: StreamServiceService,
+    private storeService: StoreService  
+  ) { }
 
   ngOnInit(): void {
     this.streamService
@@ -25,15 +30,55 @@ export class FormDrawerComponent implements OnInit, OnDestroy {
   }
 
   public open: boolean = false;
-  public action: String = '';
+  public action: string = '';
   public $destroyer: Subject<void> = new Subject<void>();
 
-  openDrawer(action: String = '') {
+  public taskForm: FormGroup = new FormGroup({
+    title: new FormControl(null, [
+      Validators.required,
+      Validators.min(4)
+    ]),
+    description: new FormControl(null),
+    labels: new FormControl([])
+  });
+
+  openDrawer(action: string = '') {
     this.open = true;
+    this.action = action;
   }
 
   closeDrawer() {
     this.open = false;
+  }
+
+  addChip(evt: any) {
+    
+    if(!evt?.value)
+      return;
+
+    const labels: Array <string> = this.taskForm.controls.labels.value;
+    labels.push(evt.value);
+    evt.value = null; 
+
+  }
+
+  removeChip(it: number) {
+    const labels: any = this.taskForm.controls.labels;
+    labels.value = labels.value.filter((node: any, index: number) => index !== it);
+  }
+
+  appendTask() {
+    
+    const { title, description, labels } = this.taskForm.value;
+    let newTask: any = {
+      title, description, labels,
+      date: Date.now(),
+      creator: 'Aditya Thakur',
+    }
+
+    this.storeService.addTask(parseInt(this.action), newTask);
+    this,this.taskForm.reset();
+    this.closeDrawer();
   }
 
   ngOnDestroy() {
