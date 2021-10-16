a5bf19977365c9ee7c04c555a3329868f4633891
Aditya Thakur <aditya.thakur.261099@gmail.com>
2021-10-16 22:26:12 +0530

    feat: profile image cmp initiated

--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -10,6 +10,40 @@ export class ProfileComponent implements OnInit {
   constructor() { }
   ngOnInit(): void { }
 
-  
+  changeProfilePicture() {
+    const inputRef: any = document.getElementById('imgStore');
+    inputRef.click();
+  }
+
+  async appendBlob(evt: any) {
+
+    const fileRef: any = evt.target;
+    const imgRef: any = document.getElementById('profileImg');
+    let url: string = URL.createObjectURL(fileRef.files[0]);
+
+    imgRef.src = url;
+    URL.revokeObjectURL(url);
+    this.extractBlob();
+
+  }
+
+  async extractBlob() {
+
+    let canvasRef: any = document.createElement('canvas');
+    const imgRef: any = document.getElementById('profileImg');
+    let ctxtRef: CanvasRenderingContext2D = canvasRef.getContext('2d');
+
+    canvasRef.width = 120;
+    canvasRef.height = 120;
+
+    let result = null;
+    ctxtRef.drawImage(imgRef, 0, 0);
+    await canvasRef.toBlob(async (res: any) => {
+      result = await res.text();
+      console.log(result);
+      return result;
+    });
+
+  }
 
 }
